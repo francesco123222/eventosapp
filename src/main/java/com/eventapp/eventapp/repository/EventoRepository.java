0056a020283511060bac9c466e39176466397699
francesco123222 <francesco123222@gmail.com>
2026-05-27 16:23:38 -0300

    Corrigindo EventoRepository

--- src/main/java/com/eventapp/eventapp/repository/EventoRepository.java
+++ src/main/java/com/eventapp/eventapp/repository/EventoRepository.java
@@ -3,6 +3,6 @@ package com.eventapp.eventapp.repository;
 import com.eventapp.eventapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EventoRepository extends CrudRepository<Evento, Long> {
+public interface EventoRepository extends CrudRepository<Evento, String> {
     Evento findByCodigo(long codigo);
 }
